85ec08a24662e686670743434a58a9138e51dbfb
alicemartynova94 <alicemartynova94@gmail.ru>
2023-12-29 19:56:27 +0300

    add service method to update a task

--- src/main/java/service/TaskService.java
+++ src/main/java/service/TaskService.java
@@ -1,19 +1,20 @@
 package service;
 
 import dao.TaskDAO;
+import domain.Status;
 import domain.Task;
-import org.hibernate.query.Query;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static java.util.Objects.isNull;
 
 @Service
 public class TaskService {
     private final TaskDAO taskDAO;
 
-    public TaskService(TaskDAO taskDAO){
+    public TaskService(TaskDAO taskDAO) {
         this.taskDAO = taskDAO;
     }
 
@@ -21,7 +22,22 @@ public class TaskService {
         return taskDAO.getAllTasks(offset, limit);
     }
 
-    public int getTotalNumberOfTasks(){
+    public int getTotalNumberOfTasks() {
         return taskDAO.getTotalNumberOfTasks();
     }
+
+    @Transactional(readOnly = false)
+    public Task update(int id, String description, Status status) {
+        Task task = taskDAO.getTaskById(id);
+        if (isNull(task)) {
+            throw new RuntimeException("Not found");
+        }
+
+        task.setDescription(description);
+        task.setStatus(status);
+        taskDAO.saveOrUpdate(task);
+        return task;
+    }
+
+   
 }
