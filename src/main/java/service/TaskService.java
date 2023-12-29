6e80451f45e3f5f310765791e084c88e811b9bab
alicemartynova94 <alicemartynova94@gmail.ru>
2023-12-29 19:40:54 +0300

    add methods to get a list of tasks or a total number of tasks

--- src/main/java/service/TaskService.java
+++ src/main/java/service/TaskService.java
@@ -1,7 +1,13 @@
 package service;
 
 import dao.TaskDAO;
+import domain.Task;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class TaskService {
@@ -9,5 +15,13 @@ public class TaskService {
 
     public TaskService(TaskDAO taskDAO){
         this.taskDAO = taskDAO;
+    }
+
+    public List<Task> getAllTasks(int offset, int limit) {
+        return taskDAO.getAllTasks(offset, limit);
+    }
+
+    public int getTotalNumberOfTasks(){
+        return taskDAO.getTotalNumberOfTasks();
     }
 }
