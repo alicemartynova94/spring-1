cc6393e82d101e7af7aeecd717facc1cdf2ce3c3
alicemartynova94 <alicemartynova94@gmail.ru>
2023-12-29 19:00:52 +0300

    create method that returns a list of limit numbers of tasks

--- src/main/java/dao/TaskDAO.java
+++ src/main/java/dao/TaskDAO.java
@@ -1,2 +1,33 @@
-package dao;public class TaskDAO {
+package dao;
+
+import domain.Task;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class TaskDAO {
+
+    private final SessionFactory sessionFactory;
+
+    public TaskDAO(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+    public List<Task> getAll(int offset, int limit) {
+        Query<Task> query = getSession().createQuery("select t from Task t", Task.class);
+        query.setFirstResult(offset);
+        query.setMaxResults(limit);
+        return query.getResultList();
+    }
+
+    private Session getSession(){
+        return sessionFactory.getCurrentSession();
+    }
 }
